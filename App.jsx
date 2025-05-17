cae7bcb198df7313e0746cbf4746aa34510c57f7
Luba225 <145285107+Luba225@users.noreply.github.com>
2025-05-17 22:15:25 +0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,26 +1,23 @@
-import React, { useRef } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import Cart from './components/Cart';
 import OrderForm from './components/OrderForm';
+import Navbar from './components/Navbar';
 import './styles/global.scss';
 
+ function App() {
+   return (
+     <div className="app">
+       <Router>
+        <Navbar />
+         <Routes>
+           <Route path="/" element={<Menu />} />
+           <Route path="/cart" element={<Cart />} />
+           <Route path="/order" element={<OrderForm />} />
+         </Routes>
+       </Router>
+     </div>
+   );
+ }
 
-function App() {
-  return (
-    <div className="app" style={{ backgroundColor: '#ffc0cb' }}>
-      <Router>
-        <nav>
-          <Link to="/">Меню</Link> | <Link to="/cart">Кошик</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Menu />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<OrderForm />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
+ export default App;
