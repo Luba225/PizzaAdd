c26927fbcb79c04f502fae82a5cec9e54f5721be
Luba225 <145285107+Luba225@users.noreply.github.com>
2025-05-17 22:21:16 +0300

    Update pizzas.js

--- pizzas.js
+++ pizzas.js
@@ -3,6 +3,7 @@ export default [
     id: 1,
     name: "Маргарита",
     description: "Сир, томати, базилік",
+    image: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
     prices: { Мала: 100, Середня: 140, Велика: 180 },
     extras: [
       { name: "Сир", price: 15 },
@@ -10,4 +11,162 @@ export default [
       { name: "Гриби", price: 10 }
     ]
   },
+  {
+    id: 2,
+    name: "Пепероні",
+    description: "Пікантна ковбаса, сир, томати",
+    image: "https://ecopizza.com.ua/555-large_default/pica-peperoni.webp",
+    prices: { Мала: 110, Середня: 150, Велика: 190 },
+    extras: [
+      { name: "Сир", price: 15 },
+      { name: "Оливки", price: 12 },
+      { name: "Перець", price: 10 }
+    ]
+  },
+  {
+    id: 3,
+    name: "Гавайська",
+    description: "Шинка, ананас, сир",
+    image: "https://cdn.pixabay.com/photo/2017/01/22/19/20/pizza-2000614_1280.jpg",
+    prices: { Мала: 120, Середня: 160, Велика: 200 },
+    extras: [
+      { name: "Шинка", price: 20 },
+      { name: "Оливки", price: 12 },
+      { name: "Ананас", price: 15 }
+    ]
+  },
+  {
+    id: 4,
+    name: "Четверо сирів",
+    description: "Моцарела, чеддер, пармезан, блакитний сир",
+    image: "https://prontopizza.ua/lviv/wp-content/uploads/sites/17/2021/07/4-syry-500x500.webp",
+    prices: { Мала: 130, Середня: 170, Велика: 210 },
+    extras: [
+      { name: "Горіх", price: 20 },
+      { name: "Дорблю", price: 20 },
+      { name: "Мед", price: 15 }
+    ]
+  },
+  {
+    id: 5,
+    name: "Капрічоза",
+    description: "Шинка, гриби, оливки, сир",
+    image: "https://vulcan.in.ua/assets/images/resources/28/reczept-piiczyi-na-drovax-kaprichoza.jpg",
+    prices: { Мала: 120, Середня: 160, Велика: 200 },
+    extras: [
+      { name: "Оливки", price: 12 },
+      { name: "Сир", price: 15 },
+      { name: "Гриби", price: 10 }
+    ]
+  },
+  {
+    id: 6,
+    name: "Чілі",
+    description: "Пікантна ковбаса, перець чилі, сир",
+    image: "https://resto-presto.com.ua/image/cache/catalog/tovars/chili21-1000x667.jpg",
+    prices: { Мала: 130, Середня: 170, Велика: 210 },
+    extras: [
+      { name: "Оливки", price: 12 },
+      { name: "Часник", price: 10 }
+    ]
+  },
+  {
+    id: 7,
+    name: "Барбекю",
+    description: "Ковбаски, соус барбекю, цибуля, сир",
+    image: "https://bc.celentano.delivery/wp-content/uploads/2020/07/barbequ-750x750.jpg",
+    prices: { Мала: 125, Середня: 165, Велика: 205 },
+    extras: [
+      { name: "Ковбаски", price: 20 },
+      { name: "Цибуля", price: 10 }
+    ]
+  },
+  {
+    id: 8,
+    name: "Римська",
+    description: "Томати, базилік, оливкова олія, стружка",
+    image: "https://cdn-media.choiceqr.com/prod-eat-romanpizza/menu/LpAKqik-cIpHsBM-zdoRvSM.webp",
+    prices: { Мала: 100, Середня: 140, Велика: 180 },
+    extras: [
+      { name: "Часник", price: 10 },
+      { name: "Оливки", price: 12 }
+    ]
+  },
+  {
+    id: 9,
+    name: "Мексиканська",
+    description: "Перець, ковбаски, оливки, чилі",
+    image: "https://panda-pizza.com.ua/cache/dish/img/dishes/pizza/mexicana.jpg?12052024",
+    prices: { Мала: 140, Середня: 180, Велика: 220 },
+    extras: [
+      { name: "Ковбаски", price: 20 },
+      { name: "Часник", price: 10 }
+    ]
+  },
+  {
+    id: 10,
+    name: "Вегетаріанська",
+    description: "Овочі, гриби, сир",
+    image: "https://cipollino.ua/content/uploads/images/vegan-pizza-recipe-cipollino.ua.jpg",
+    prices: { Мала: 110, Середня: 150, Велика: 190 },
+    extras: [
+      { name: "Огірки", price: 15 },
+      { name: "Гриби", price: 10 }
+    ]
+  },
+  {
+    id: 11,
+    name: "Морська",
+    description: "Риба, морепродукти, сир",
+    image: "https://pizza-sushi.com.ua/sites/default/files/styles/wm_style/public/content/blyudo/blyudo-preview-images/picca-morskaya-irpen-bucha-pizza-s-moreproduktami-zakaz-dostavka-edy.jpg?itok=-g8iXrE2",
+    prices: { Мала: 150, Середня: 190, Велика: 230 },
+    extras: [
+      { name: "Креветки", price: 30 },
+      { name: "Морепродукти", price: 25 }
+    ]
+  },
+  {
+    id: 12,
+    name: "Баварська",
+    description: "Сосиски, ковбаски, цибуля, сир",
+    image: "https://pablito.com.ua/wp-content/uploads/2017/11/Pitstsa_Bavarskaya.png",
+    prices: { Мала: 130, Середня: 170, Велика: 210 },
+    extras: [
+      { name: "Гриби", price: 10 },
+      { name: "Цибуля", price: 12 }
+    ]
+  },
+  {
+    id: 13,
+    name: "Швидка",
+    description: "Шинка, ковбаски, помідори, сир",
+    image: "https://cdn-media.choiceqr.com/prod-eat-romanpizza/menu/eqQuHKa-bexcYVt-lTHHCDn.webp",
+    prices: { Мала: 115, Середня: 155, Велика: 195 },
+    extras: [
+      { name: "Шинка", price: 15 },
+      { name: "Гриби", price: 10 }
+    ]
+  },
+  {
+    id: 14,
+    name: "Тоскана",
+    description: "Ковбаски, оливки, помідори, сир",
+    image: "https://cipollino.ua/content/uploads/images/pytstsa-toskana-liubymoe-bliudo-v-novom-prochtenyy-cipollino.jpg",
+    prices: { Мала: 130, Середня: 170, Велика: 210 },
+    extras: [
+      { name: "Оливки", price: 12 },
+      { name: "Помідори", price: 10 }
+    ]
+  },
+  {
+    id: 15,
+    name: "Аліканте",
+    description: "Томати, базилік, оливкова олія, сир",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2e/0d/b1/0c/pizza-burrata.jpg?w=1400&h=800&s=1",
+    prices: { Мала: 120, Середня: 160, Велика: 200 },
+    extras: [
+      { name: "Часник", price: 10 },
+      { name: "Оливки", price: 12 }
+    ]
+  }
 ];
